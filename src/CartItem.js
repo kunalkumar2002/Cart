85e8769afea54179597b8abb31fe740d23e3edee
Kunal Kumar <kunalkumar7091679623@gmail.com>
2023-06-12 13:56:19 +0530

    add increaseQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,52 @@
 import React from "react";
 
 class CartItem extends React.Component{
+    
+    constructor(){
+        super();
+        this.state = {
+            title :'Mobile Phone',
+            price : 999,
+            qty : 1,
+            img : ''
+        }
+        //we can also bind here 
+        // this.increaseQuantity = this.increaseQuantity.bind(this)
+    }
+
+    // increaseQuantity(){
+    //     console.log('this',this)
+    // }
+    //or in place of this we can use arrow function that willl automaticly Bind
+
+    increaseQuantity = () => {
+        console.log('this.state' , this.state)
+    }
+
     render(){
+
+        const {title , price , qty} = this.state;
+
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>rs 190000</div>
-                    <div style={{color:'#777'}}>Qty. 1</div>
-                    <div className="cart-item-action">
-                        {/*button */}
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>rs {price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
+                    <div className="cart-item-actions">
+                        {/*button */} 
+                        <img 
+                           alt="increase" 
+                           className="action-icons" 
+                           src="https://cdn-icons-png.flaticon.com/128/3303/3303893.png"
+                           onClick={this.increaseQuantity}
+                            // onClick={this.increaseQuantity.bind(this)} 
+                        />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" />
                     </div>
                 </div>
             </div>
