bd9bc2fb986e9c8dad23787c08d7d5af80e9d520
Kunal Kumar <kunalkumar7091679623@gmail.com>
2023-06-21 02:57:15 +0530

    add  total price

--- src/App.js
+++ src/App.js
@@ -13,80 +13,91 @@ class App extends React.Component{
                 title :'Mobile Phone',
                 price : 999,
                 qty : 1,
-                img : ''  ,
+                img : 'https://m.media-amazon.com/images/I/51l-iIutivL._AC_UF1000,1000_QL80_.jpg'  ,
                 id: 1,
             },
             {
                 title :'Watch',
                 price : 99,
                 qty : 2,
-                img : ''  ,
+                img : 'https://images.pexels.com/photos/2113994/pexels-photo-2113994.jpeg?cs=srgb&dl=pexels-joey-nguy%E1%BB%85n-2113994.jpg&fm=jpg'  ,
                 id: 2,
             },
             {
                 title :'Leptop',
                 price : 999,
                 qty : 1,
-                img : ''  ,
+                img : 'https://web.s-cdn.boostkit.dev/webaction-files/5ac4f587beef72050ad4e284_product_images/2a7dbfd4-9018-46e5-88d8-7767bad952c4-60fa1d9c67e30f00018f1f0f.png'  ,
                 id: 3,
             }
         ]
     }
     //we can also bind here 
     // this.increaseQuantity = this.increaseQuantity.bind(this)
-}
+  }
 
-handleIncreaseQuantity = (product) =>{
-    // console.log('heyy increase product quantity' , product)
+  handleIncreaseQuantity = (product) =>{
+      // console.log('heyy increase product quantity' , product)
+      const { products } = this.state;
+      const index = products.indexOf(product);
+
+      products[index].qty += 1;
+
+      this.setState({
+          products : products
+      })
+  }
+
+  handleDecreaseQuantity = (product) =>{
+      //console.log('hay decrease qty ' , product);
+
+      const {products} = this.state;
+      const index = products.indexOf(product);
+
+      if(products[index].qty === 0){
+          return ;
+      }
+
+      products[index].qty -= 1;
+
+      this.setState({
+          products : products
+      })
+  }
+
+  handleDeleteProduct = (id) =>{
+
+      const {products} = this.state;
+
+      const item = products.filter((item) => 
+          item.id !== id
+      )
+
+      this.setState({
+          products :item
+      })
+  }
+  getcartCount = () => {
+    const {products} = this.state;
+
+    let count = 0 ;
+    products.forEach((item) => {
+      count += item.qty;
+    })
+
+    return count;
+  }
+
+  totalPrice = () => {
     const { products } = this.state;
-    const index = products.indexOf(product);
-
-    products[index].qty += 1;
-
-    this.setState({
-        products : products
+    
+    let cartTotal = 0;
+    products.map((product) =>{
+      cartTotal = cartTotal + product.qty*product.price;
     })
-}
-
-handleDecreaseQuantity = (product) =>{
-    //console.log('hay decrease qty ' , product);
-
-    const {products} = this.state;
-    const index = products.indexOf(product);
-
-    if(products[index].qty === 0){
-        return ;
-    }
-
-    products[index].qty -= 1;
-
-    this.setState({
-        products : products
-    })
-}
-
-handleDeleteProduct = (id) =>{
-
-    const {products} = this.state;
-
-    const item = products.filter((item) => 
-        item.id !== id
-    )
-
-    this.setState({
-        products :item
-    })
-}
-getcartCount = () => {
-  const {products} = this.state;
-
-  let count = 0 ;
-  products.forEach((item) => {
-    count += item.qty;
-  })
-
-  return count;
-}
+    
+    return cartTotal;
+  }
 
   render(){
 
@@ -102,7 +113,11 @@ getcartCount = () => {
            onDecreaseQuantity = {this.handleDecreaseQuantity}
            onDeleteProduct = {this.handleDeleteProduct}
         />
+
+        <div style={{padding: 10 , fontSize: 20}} >Total: {this.totalPrice()} </div>
+
       </div>
+
     );
   }
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -59,7 +59,7 @@ const CartItem = (props) => {
     return (
         <div className="cart-item">
             <div className="left-block">
-                <img style={styles.image} />
+                <img style={styles.image} src={product.img} />
             </div>
             <div className="right-block">
                 <div style={{fontSize:20}}>{title}</div>
